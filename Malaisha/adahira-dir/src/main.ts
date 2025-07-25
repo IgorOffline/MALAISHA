10f0adb4f8687a271cb8eb80dad45cd9db2b4ead
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2025-07-25 08:28:23 +0200

    Dev4 (#4)
    * +album
    
    * +gitignore

--- Malaisha/adahira-dir/src/main.ts
+++ Malaisha/adahira-dir/src/main.ts
@@ -1,10 +1,33 @@
 const urlApiAlbum = '/api/album';
 
+class Album {
+    id: number;
+    name: string;
+    ctime: string;
+    mtime: string;
+
+    constructor(id: number, name: string, ctime: string, mtime: string) {
+        this.id = id;
+        this.name = name;
+        this.ctime = ctime;
+        this.mtime = mtime;
+    }
+}
+
 async function malaishaGetAlbum() {
     const responseRaw = await fetch(urlApiAlbum);
     const response = await responseRaw.json();
 
     console.log("response= ", response);
+
+    const album = new Album(
+        response.id,
+        response.name,
+        response.ctime,
+        response.mtime
+    );
+
+    console.log("album= ", album);
 }
 
 export function malaishaReady(fn: (this: Document, ev: Event) => any) {
